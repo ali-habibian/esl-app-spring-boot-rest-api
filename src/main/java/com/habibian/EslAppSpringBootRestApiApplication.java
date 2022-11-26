0752d7a1d81b87698fe3125cdfae4dbc02bd36f6
Ali Habibian <ali.habibian04@gmail.com>
2022-11-26 21:33:30 +0330

    Initial commit

---
+++ src/main/java/com/habibian/EslAppSpringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.habibian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EslAppSpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EslAppSpringBootRestApiApplication.class, args);
+    }
+
+}
