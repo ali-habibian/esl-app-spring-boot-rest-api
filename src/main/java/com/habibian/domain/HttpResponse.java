0c312b3853b7af1788773dd3f6eca558c04c21aa
Ali Habibian <ali.habibian04@gmail.com>
2022-12-04 19:00:49 +0330

    Added HttpResponse class in domain package

---
+++ src/main/java/com/habibian/domain/HttpResponse.java
@@ -0,0 +1,29 @@
+package com.habibian.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class HttpResponse {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Tehran")
+    private Date timestamp;
+    private int httpStatusCode;
+    private HttpStatus httpStatus;
+    private String reason;
+    private String message;
+
+    public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timestamp = new Date();
+        this.httpStatusCode = httpStatusCode;
+        this.httpStatus = httpStatus;
+        this.reason = reason;
+        this.message = message;
+    }
+}
