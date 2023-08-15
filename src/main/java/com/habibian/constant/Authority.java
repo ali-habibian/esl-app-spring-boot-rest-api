e118fb20073b0115aea5ac90df8a53e754bbe501
Ali Habibian <ali.habibian04@gmail.com>
2023-08-16 00:17:19 +0330

    Added all authorities

--- src/main/java/com/habibian/constant/Authority.java
+++ src/main/java/com/habibian/constant/Authority.java
@@ -8,11 +8,32 @@ package com.habibian.constant;
 public class Authority {
 
     // Authority array for regular users
-    public static final String[] USER_AUTHORITIES = {"user:read"};
+    public static final String[] USER_AUTHORITIES = {
+            "user:read-self", "user:update-self",
+            "listeningLesson:read",
+            "vocab:read",
+            "exampleSentence:read",
+            "gapFillQuiz:read",
+            "multipleChoiceQuiz:read"
+    };
 
     // Authority array for admin users
-    public static final String[] ADMIN_AUTHORITIES = {"user:read", "user:create", "user:update"};
+    public static final String[] ADMIN_AUTHORITIES = {
+            "user:read", "user:create", "user:update",
+            "listeningLesson:read", "listeningLesson:create", "listeningLesson:update", "listeningLesson:delete",
+            "vocab:read", "vocab:create", "vocab:update", "vocab:delete",
+            "exampleSentence:read", "exampleSentence:create", "exampleSentence:update", "exampleSentence:delete",
+            "gapFillQuiz:read", "gapFillQuiz:create", "gapFillQuiz:update", "gapFillQuiz:delete",
+            "multipleChoiceQuiz:read", "multipleChoiceQuiz:create", "multipleChoiceQuiz:update", "multipleChoiceQuiz:delete"
+    };
 
     // Authority array for super admin users
-    public static final String[] SUPER_ADMIN_AUTHORITIES = {"user:read", "user:create", "user:update", "user:delete"};
+    public static final String[] SUPER_ADMIN_AUTHORITIES = {
+            "user:read", "user:create", "user:update", "user:delete",
+            "listeningLesson:read", "listeningLesson:create", "listeningLesson:update", "listeningLesson:delete",
+            "vocab:read", "vocab:create", "vocab:update", "vocab:delete",
+            "exampleSentence:read", "exampleSentence:create", "exampleSentence:update", "exampleSentence:delete",
+            "gapFillQuiz:read", "gapFillQuiz:create", "gapFillQuiz:update", "gapFillQuiz:delete",
+            "multipleChoiceQuiz:read", "multipleChoiceQuiz:create", "multipleChoiceQuiz:update", "multipleChoiceQuiz:delete"
+    };
 }
