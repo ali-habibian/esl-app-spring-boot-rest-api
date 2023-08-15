5387b696dab2e3c62f4ecbe83418c745cd9b269c
Ali Habibian <ali.habibian04@gmail.com>
2023-08-16 00:58:27 +0330

    fixed bugs in authorities

--- src/main/java/com/habibian/constant/Authority.java
+++ src/main/java/com/habibian/constant/Authority.java
@@ -9,31 +9,16 @@ public class Authority {
 
     // Authority array for regular users
     public static final String[] USER_AUTHORITIES = {
-            "user:read-self", "user:update-self",
-            "listeningLesson:read",
-            "vocab:read",
-            "exampleSentence:read",
-            "gapFillQuiz:read",
-            "multipleChoiceQuiz:read"
+            "role:read"
     };
 
     // Authority array for admin users
     public static final String[] ADMIN_AUTHORITIES = {
-            "user:read", "user:create", "user:update",
-            "listeningLesson:read", "listeningLesson:create", "listeningLesson:update", "listeningLesson:delete",
-            "vocab:read", "vocab:create", "vocab:update", "vocab:delete",
-            "exampleSentence:read", "exampleSentence:create", "exampleSentence:update", "exampleSentence:delete",
-            "gapFillQuiz:read", "gapFillQuiz:create", "gapFillQuiz:update", "gapFillQuiz:delete",
-            "multipleChoiceQuiz:read", "multipleChoiceQuiz:create", "multipleChoiceQuiz:update", "multipleChoiceQuiz:delete"
+            "role:read", "role:create", "role:update", "role:delete",
     };
 
     // Authority array for super admin users
     public static final String[] SUPER_ADMIN_AUTHORITIES = {
-            "user:read", "user:create", "user:update", "user:delete",
-            "listeningLesson:read", "listeningLesson:create", "listeningLesson:update", "listeningLesson:delete",
-            "vocab:read", "vocab:create", "vocab:update", "vocab:delete",
-            "exampleSentence:read", "exampleSentence:create", "exampleSentence:update", "exampleSentence:delete",
-            "gapFillQuiz:read", "gapFillQuiz:create", "gapFillQuiz:update", "gapFillQuiz:delete",
-            "multipleChoiceQuiz:read", "multipleChoiceQuiz:create", "multipleChoiceQuiz:update", "multipleChoiceQuiz:delete"
+            "role:read", "role:create", "role:update", "role:delete", "role:s-admin"
     };
 }
